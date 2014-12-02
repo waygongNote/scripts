abc656a582f58319df18d866e936f84470e58ced
waygongNote <waygongNote@users.noreply.github.com>
2014-12-02 15:54:21 +0800

    Update catFunc.sh

--- catFunc.sh
+++ catFunc.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 #rm -r ~/traceDir (if it was created by yourself before)
-test -e ~/traceDir && echo "~/traceDir exist" && exit 0
+test -e ~/traceDir && echo "(~/traceDir exist)" && exit 0
 
-mkdir ~/traceDir
-#for safe (avoid that you are in /root/, and then ...)
-cd ~/traceDir
-#focusDir=...(define by yourself)
-cd $fucusDir
+originDir=`pwd`
+echo "original directory = `pwd`" ###
+cd ~
+traceDir=traceDir
+mkdir $traceDir
+cd $traceDir #to avoid problems if the following dirctory doens't exist
+echo "trace directory = `pwd`" ###
+#focusDir=... (please type in the directory you focus)
+test ! -e $focusDir && echo "(~/focusDir doesn't exist)" && exit 0
+cd $focusDir
+echo "focus directory = `pwd`" ###
+
+#if [  ];then
 rsync -a --include '*/' --include '*.c' --exclude '*' . ~/traceDir
 cd ~/traceDir
 function function_name ()
 {
-	#be care of the 80 length limit (waiting to modify)
-	test ! -d $filename && cat "$filename" | grep "^[a-z|A-Z]\|{\|}\|#\|goto\|case\|if(\|if\ (\|else\|while(\|while\ (\|for(\|for\ (\|do(\|do\ (\|\ \ return[;\ ]\|^\ return[;\ ]\|\ \ break;\|^\ break;\|default:\|)$" | grep -v -E "^\s+\*" > "$filename".temp.c
+    #be care of the 80 length limit (waiting to modify, but it's correct to me now)
+    test ! -d $filename && cat "$filename" | grep "^[a-z|A-Z]\|{\|}\|#\|goto\|case\|if(\|if\ (\|else\|while(\|while\ (\|for(\|for\ (\|do(\|do\ (\|[[:blank:]][[:blank:]]return[;\ ]\|^[[:blank:]]return[;\ ]\|\ \ break;\|^\ break;\|default:\|)$\|/\*\|\*/\|[[:blank:]][[:blank:]]\*\|^[[:blank:]]\*" > "$filename".temp.c
 }
-for filename in $(ls | sed "s:^:`pwd`/:")
+#for filename in $(ls -R| sed "s:^:`pwd`/:"|grep \.c$) => (it's not good enough, the below is better)
+for filename in $(find $pwd | grep \.c$ | grep "net\.c\|telnet")
 do
-	function_name
+    function_name
 done
-
-cd -
+for filename in $(find $pwd | grep \.c$ | grep -v "net\.c\|telnet")
+do
+    rm -f $filename
+done
+#fi
