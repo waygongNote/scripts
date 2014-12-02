669528e574b824c40d765ca71d398bf65a7ce080
waygongNote <waygongNote@users.noreply.github.com>
2014-12-02 15:57:34 +0800

    Update catFunc.sh

--- catFunc.sh
+++ catFunc.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-#rm -r ~/traceDir (if it was created by yourself before)
-test -e ~/traceDir && echo "(~/traceDir exist)" && exit 0
+#rm -r $HOME/traceDir (if it was created by yourself before)
+test -e $HOME/traceDir && echo "($HOME/traceDir exist)" && exit 0
 
 originDir=`pwd`
 echo "original directory = `pwd`" ###
-cd ~
+cd $HOME
 traceDir=traceDir
 mkdir $traceDir
 cd $traceDir #to avoid problems if the following dirctory doens't exist
 echo "trace directory = `pwd`" ###
 #focusDir=... (please type in the directory you focus)
-test ! -e $focusDir && echo "(~/focusDir doesn't exist)" && exit 0
+test ! -e $focusDir && echo "($HOME/focusDir doesn't exist)" && exit 0
 cd $focusDir
 echo "focus directory = `pwd`" ###
 
 #if [  ];then
-rsync -a --include '*/' --include '*.c' --exclude '*' . ~/traceDir
-cd ~/traceDir
+rsync -a --include '*/' --include '*.c' --exclude '*' . $HOME/traceDir
+cd $HOME/traceDir
 function function_name ()
 {
     #be care of the 80 length limit (waiting to modify, but it's correct to me now)
