185aada3a58ee2d2ab510d99bb258ce34d31f7b5
waygongNote <waygongNote@users.noreply.github.com>
2014-12-05 10:19:21 +0800

    Create alarm.sh

---
+++ alarm.sh
@@ -0,0 +1,9 @@
+# fail to do below commands... (because of not root?)
+# {{{
+# intro: http://linux.vbird.org/linux_basic/0430cron.php
+# ps | grep "TTY\|ps" | grep "TTY\|pts/[0-9]" --color
+# crontab -l|tail -2
+# crontab -e
+# */5 9-18 * * * clear;echo hello!!> /dev/tty1;`date`;
+# */5 9-18 * * * clear;write `whoami` `tty`;
+# }}}
