9868437db76354ee5789031328dab2c2d536857f
U-CAMEO\Wayne_Huang <waygongnote@gmail.com>
2014-12-19 11:53:53 +0800

    du -sh (in KB WITH decimal)

---
+++ du_sh_KB_with_decimal.sh
@@ -0,0 +1,15 @@
+# there's some problem when using du -sb (?) != du -s/1024
+size=$(du -s|awk '{print $1}')
+echo -n $size" ("
+#echo "scale=6;$(echo $(du -s|awk '{print $1}'))/1024" |bc
+if [ $size -ge $((1024*1024)) ]; then
+	printf $(echo "scale=3;$size/1024/1024" |bc)
+	echo -n G
+elif [ $size -ge $((1024)) ]; then
+	printf $(echo "scale=3;$size/1024" |bc)
+	echo -n M
+else
+	echo -n $size
+	echo -n K
+fi
+echo ")"
