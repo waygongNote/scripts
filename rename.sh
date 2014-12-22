a72d0475d3452b70ff7cd4fe4613fe051c114d9d
U-CAMEO\Wayne_Huang <waygongnote@gmail.com>
2014-12-22 19:42:22 +0800

    a ci

---
+++ rename.sh
@@ -0,0 +1,5 @@
+
+#if [ $# == 1 ] && [ $(echo $0 | tail -c 17) == "filename_date.sh" ]; then
+#	echo $1
+#	cp -a $1 $(echo $1 | cut -d '.' -f 1)_`echo $(date +%Y)_$(date +%m%d)_$(date +%R | head -c 2)$(date +%R | tail -c 3)`.log
+#fi
